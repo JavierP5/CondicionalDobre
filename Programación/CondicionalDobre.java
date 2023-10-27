58613e7d6fc970ff8f687e8b0d334f06251db13b
dam1 <dam1@Aulainformatica32>
2023-10-27 08:50:07 +0200

    Terminado boletin

---
+++ Programación/CondicionalDobre.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exemplocondicionais;
+
+/**
+ *
+ * @author dam1
+ */
+public class CondicionalDobre {
+    
+    // metodo que recibe dous numeros distintos e visualiza cal e o maior
+    public void amosarMaior(int num1,int num2){
+        
+       if(num1>num2)
+           System.out.println(num1 + "e maior");
+       else
+           System.out.println(num2 + "e maior");
+    }
+    
+}
